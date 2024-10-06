78c3075520732d305953dfcf7af1f79a6ec831e1
ae00 <ae00@users.noreply.github.com>
2024-10-06 10:42:23 +0200

    Script to toggle transparency on focused window

---
+++ .config/i3/trans.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+command = "picom-trans --current --get"
+
+output = subprocess.check_output(command, shell=True, universal_newlines=True)
+
+print(output)
+
+if "100" in output:
+	os.system("picom-trans --current --opacity 30")
+else:
+	os.system("picom-trans --current --opacity 100")
