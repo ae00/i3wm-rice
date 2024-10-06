ea7d9affbfd170d9312e4919116cd499ccc950d4
ae00 <ae00@users.noreply.github.com>
2024-10-06 12:16:40 +0200

    Removed line

--- .config/i3/trans.py
+++ .config/i3/trans.py
@@ -4,8 +4,6 @@ command = "picom-trans --current --get"
 
 output = subprocess.check_output(command, shell=True, universal_newlines=True)
 
-print(output)
-
 if "100" in output:
 	os.system("picom-trans --current --opacity 40")
 else:
