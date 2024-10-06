ff75a92e469e797934857850e256aeafa1acd2cd
ae00 <ae00@users.noreply.github.com>
2024-10-06 11:21:16 +0200

    Changed opacity level

--- .config/i3/trans.py
+++ .config/i3/trans.py
@@ -7,6 +7,6 @@ output = subprocess.check_output(command, shell=True, universal_newlines=True)
 print(output)
 
 if "100" in output:
-	os.system("picom-trans --current --opacity 30")
+	os.system("picom-trans --current --opacity 40")
 else:
 	os.system("picom-trans --current --opacity 100")
