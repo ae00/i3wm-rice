19f827f58a9425a5e533360415bae01019095a88
ae00 <ae00@users.noreply.github.com>
2024-10-23 18:11:24 +0200

    Added image download command and view image in feh command

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,5 @@
 config.set("colors.webpage.darkmode.enabled", True)
 config.load_autoconfig()
 config.bind(',M', 'hint links spawn mpv --profile=qutebrowser {hint-url}')
+config.bind(',I', 'hint images download')
+config.bind(',i', 'hint images spawn feh {hint-url}')
