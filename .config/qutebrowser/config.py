a2bbc67ca12c9af019fc8e95548b0279ccdddd60
ae00 <ae00@users.noreply.github.com>
2024-11-06 19:00:41 +0100

    Added ranger as the filepicker

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,3 +3,7 @@ config.load_autoconfig()
 config.bind(',M', 'hint links spawn mpv --profile=qutebrowser {hint-url}')
 config.bind(',I', 'hint images download')
 config.bind(',i', 'hint images spawn feh {hint-url}')
+
+config.set("fileselect.handler", "external")
+config.set("fileselect.single_file.command", ['alacritty', '--class', 'ranger,ranger', '-e', 'ranger', '--choosefile', '{}'])
+config.set("fileselect.multiple_files.command", ['alacritty', '--class', 'ranger,ranger', '-e', 'ranger', '--choosefiles', '{}'])
