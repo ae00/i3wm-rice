a79e4197e220739a35c2cf4d38ef9bd4161209e3
ae00 <ae00@users.noreply.github.com>
2025-06-01 15:46:32 +0200

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,4 +1,4 @@
-config.set("colors.webpage.darkmode.enabled", True)
+#config.set("colors.webpage.darkmode.enabled", True)
 config.load_autoconfig()
 config.bind(',M', 'hint links spawn mpv --profile=qutebrowser {hint-url}')
 config.bind(',I', 'hint images download')
@@ -7,3 +7,6 @@ config.bind(',i', 'hint images spawn feh {hint-url}')
 config.set("fileselect.handler", "external")
 config.set("fileselect.single_file.command", ['alacritty', '--class', 'ranger,ranger', '-e', 'ranger', '--choosefile', '{}'])
 config.set("fileselect.multiple_files.command", ['alacritty', '--class', 'ranger,ranger', '-e', 'ranger', '--choosefiles', '{}'])
+# xterm
+#config.set("fileselect.single_file.command", ["xterm", "-e", "ranger", "--choosefile={}"])
+#config.set("fileselect.multiple_files.command", ["xterm", "-e", "ranger", "--choosefile={}"])
